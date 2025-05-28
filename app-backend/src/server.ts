88b01c261784890b3067d6c50aee950aa6f30a11
Samuel Caetite <srcaetite@gmail.com>
2025-05-28 08:58:27 -0300

    refactor: add path to docs to env file

--- app-backend/src/server.ts
+++ app-backend/src/server.ts
@@ -7,11 +7,13 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI =
   process.env.MONGO_URI || "mongodb://localhost:27017/crypto-api";
+const DOCS_URI = process.env.DOCS_URI;
 
 mongoose
   .connect(MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
+    console.log(`API Documentation available in ${DOCS_URI}`);
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((error) => console.error("MongoDB connection error:", error));
